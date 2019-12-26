a13cde9e4f37a9d63ec5611d9cb661475ebfc051
Kinzerfest <kinzerfest@gmail.com>
2019-12-25 21:12:05 -0600

    Added real life kinematics, made roomba communication a thing
    I also messed everything up with lambdas

--- src/main/java/org/minutebots/frc2019/Robot.java
+++ src/main/java/org/minutebots/frc2019/Robot.java
@@ -1,97 +1,73 @@
 package org.minutebots.frc2019;
 
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
-import org.minutebots.lib.controller.PIDController;
 import org.minutebots.lib.controller.RamseteController;
-import org.minutebots.lib.controller.SimpleMotorFeedforward;
 import org.minutebots.lib.geometry.Pose2d;
 import org.minutebots.lib.geometry.Rotation2d;
 import org.minutebots.lib.geometry.Translation2d;
 import org.minutebots.lib.kinematics.DifferentialDriveKinematics;
 import org.minutebots.lib.kinematics.DifferentialDriveOdometry;
-import org.minutebots.lib.kinematics.DifferentialDriveWheelSpeeds;
-import org.minutebots.lib.trajectory.Trajectory;
 import org.minutebots.lib.trajectory.TrajectoryConfig;
 import org.minutebots.lib.trajectory.TrajectoryGenerator;
-
 import java.util.ArrayList;
-import java.util.function.Supplier;
-
 public class Robot extends TimedRobot {
-    Encoder leftEncoder = new Encoder(0, 0);
-    Encoder rightEncoder = new Encoder(0, 0);
-    DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(new Rotation2d(0.0));
-    private Supplier<Pose2d> getPose = new Supplier<Pose2d>() {
-
-        public Pose2d get() {
-            return m_odometry.update(new Rotation2d(gyroAngle()), leftEncoder.getRate(), rightEncoder.getRate());
-        }
-
-    };
 
     /**
-     * This function is run when the robot is first started up and should be used
-     * for any initialization code.
-     */
-    @Override
-    public void robotInit() {
+     * This code runs on a virtual robot environment on the driver station PC, and communicates with a Roomba through NetworkTables.
+     * While performance offboard may have more latency, this is the only way I know of getting NI Driverstation Packets, through the loopback DS socket.
+     *
+     *We'll set up NetworkTable values to the Roomba.
+     **/
 
+    static { //static blocks cursed?
+        NetworkTableInstance ntInstance = NetworkTableInstance.create();
+        ntInstance.startClient("frcvision.local");
     }
 
-    private Ramsete generateRamsete() {
-        Pose2d start = new Pose2d();
-        Pose2d end = new Pose2d();
-        TrajectoryConfig config = new TrajectoryConfig(1.0, 1.0);
-        ArrayList<Translation2d> interiorWaypoints = new ArrayList<Translation2d>();
-        SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0, 0);
-        DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(0);
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("Roomba");
+    NetworkTableEntry leftMotor = table.getEntry("leftMotor"),
+            rightMotor = table.getEntry("rightMotor"),
+            leftRate = table.getEntry("leftRate"),
+            rightRate = table.getEntry("rightRate"),
+            angle = table.getEntry("angle");
 
-        PIDController leftController = new PIDController(0, 0, 0);
-        PIDController rightController = new PIDController(0, 0, 0);
+    DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(new Rotation2d(angle.getDouble(0)));
+    
+    /**
+     * The Roomba is actually smart enough to follow paths given raw wheel speeds, so we'll use the simplified RAMSETE constructor.
+     *
+     * Also, I've finally decided to switch to the metric system, so all the lengths/speeds are in metres.
+     **/
 
+    //TRAJECTORY CONSTRAINTS
+    TrajectoryConfig config = new TrajectoryConfig(0.5, 0.2);
 
-        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(start, interiorWaypoints, end, config);
+    //TRAJECTORY WAYPOINTS
+    Pose2d start = new Pose2d();
+    Pose2d end = new Pose2d(); //TODO: Add a test trajectory
+    ArrayList<Translation2d> interiorWaypoints = new ArrayList<>();
 
-        RamseteController controller = new RamseteController();
-
-        return new Ramsete(trajectory, getPose, controller, feedforward, kinematics, this::wheelSpeeds, leftController, rightController, this::voltConsumer);
-    }
-
-    private DifferentialDriveWheelSpeeds wheelSpeeds() {
-        return new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate());
-    }
-
-    private void voltConsumer(Double voltsLeft, Double voltsRight) {
-      //We don't have a use for left and right volts as of the moment.
-    }
-
-    private double gyroAngle() {
-        return 0;
-    }
+    Ramsete ramsete = new Ramsete(
+            TrajectoryGenerator.generateTrajectory(start, interiorWaypoints, end, config),
+            () -> m_odometry.update(new Rotation2d(angle.getDouble(0)), leftRate.getDouble(0), rightRate.getDouble(0)),
+            new RamseteController(),
+            new DifferentialDriveKinematics(0.235), //Taken off iCreate2 Manual,
+            (left, right) -> {
+                //After running through calculations, this block will be executed by the Ramsete Controller, turning the wheels.
+                leftMotor.setDouble(left);
+                rightMotor.setDouble(right);
+            });
 
     @Override
     public void autonomousInit() {
+        ramsete.initialize();
     }
 
     @Override
     public void autonomousPeriodic() {
+        if (!ramsete.isFinished()) ramsete.execute();
     }
-
-    @Override
-    public void teleopInit() {
-    }
-
-    @Override
-    public void teleopPeriodic() {
-    }
-
-    @Override
-    public void testInit() {
-    }
-
-    @Override
-    public void testPeriodic() {
-    }
-
 }
